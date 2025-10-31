4de9760ae25eef282fc3701bb0e464be007abd5b
raiiden15 <pawnikarvarenya@gmail.com>
2025-10-31 15:31:56 +0530

    3_auth

--- test.js
+++ test.js
@@ -1,8 +1,11 @@
 // test-env.js
-console.log('--- Environment Check ---');
+console.log("--- Environment Check ---");
 if (process.env.DATABASE_URL) {
-  console.log('DATABASE_URL is loaded! The value starts with:', process.env.DATABASE_URL.substring(0, 20) + '...');
+  console.log(
+    "DATABASE_URL is loaded! The value starts with:",
+    process.env.DATABASE_URL.substring(0, 20) + "...",
+  );
 } else {
-  console.log('DATABASE_URL is NOT loaded.');
+  console.log("DATABASE_URL is NOT loaded.");
 }
-console.log('-------------------------');
+console.log("-------------------------");
